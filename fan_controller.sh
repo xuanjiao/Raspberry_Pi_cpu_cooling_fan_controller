a5a1101158f78be4d0e8aacfd50a600c90b99d1f
xuanjiao <skaterxjz@gmail.com>
2021-01-01 18:34:39 +0100

    write fan controll script

---
+++ fan_controller.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+gpio mode 15 out
+
+
+while [ 1 ]
+
+do
+read DATA < /sys/class/thermal/thermal_zone0/temp
+TEMP=`expr ${DATA} / 1000`
+echo -e "Temperature $TEMP" 
+
+# Turn on the fan if the temperature is above 43 degree
+if [ $TEMP -gt 43 ]; then
+	gpio write 15 0 	
+fi
+#Turn off the fan if the temperature is less then 37 degree
+if [ $TEMP -lt 37 ]; then
+	gpio write 15 1
+fi
+
+sleep 10
+
+done
