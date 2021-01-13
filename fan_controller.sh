0adfe79418e79edd03662a57024449d632c04793
xuanjiao <skaterxjz@gmail.com>
2021-01-13 11:02:12 +0100

    change sleep time and temp threshold

--- fan_controller.sh
+++ fan_controller.sh
@@ -9,8 +9,8 @@ read DATA < /sys/class/thermal/thermal_zone0/temp
 TEMP=`expr ${DATA} / 1000`
 echo -e "Temperature $TEMP" 
 
-# Turn on the fan if the temperature is above 43 degree
-if [ $TEMP -gt 43 ]; then
+# Turn on the fan if the temperature is above 45 degree
+if [ $TEMP -gt 45 ]; then
 	gpio write 15 0 	
 fi
 #Turn off the fan if the temperature is less then 37 degree
@@ -18,6 +18,6 @@ if [ $TEMP -lt 37 ]; then
 	gpio write 15 1
 fi
 
-sleep 10
+sleep 30
 
 done
